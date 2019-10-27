1e7954288b3181236918024b115a7cedade27740
alex@decormatters.com <alex@decormatters.com>
2019-10-26 20:51:39 -0700

    Initial Commit

---
+++ MyGrabCut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGrabCut
+//
+//  Created by alex on 10/26/19.
+//  Copyright © 2019 alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
